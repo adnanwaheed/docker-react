edf9603053a0458648c30695d0d06badf6fc3154
Adnan Waheed <adnanwaheed@users.noreply.github.com>
2019-07-22 19:15:24 +0200

    Test message

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello there
+          General Kenobi
         </p>
         <a
           className="App-link"
